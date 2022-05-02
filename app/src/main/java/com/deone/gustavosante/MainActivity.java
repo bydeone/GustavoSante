bf02b75b368105e0ed808cffe9b8721800a4a2c0
deone <ndjanda@gmail.com>
2022-05-02 13:59:45 +0100

    Création de l'interface de connexion à l'application Gustavo Santé

--- app/src/main/java/com/deone/gustavosante/MainActivity.java
+++ app/src/main/java/com/deone/gustavosante/MainActivity.java
@@ -2,13 +2,42 @@ package com.deone.gustavosante;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.deone.gustavosante.connexion.ConnexionActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private String myuid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        checkUser();
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    private void checkUser() {
+        FirebaseUser fUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (fUser != null){
+            myuid = fUser.getUid();
+            initViews();
+        }else {
+            startActivity(new Intent(this, ConnexionActivity.class));
+            finish();
+        }
+    }
+
+    private void initViews() {
+
     }
 }
---
+++ app/src/main/java/com/deone/gustavosante/connexion/ConnexionActivity.java
@@ -0,0 +1,16 @@
+package com.deone.gustavosante.connexion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.deone.gustavosante.R;
+
+public class ConnexionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_connexion);
+    }
+}
